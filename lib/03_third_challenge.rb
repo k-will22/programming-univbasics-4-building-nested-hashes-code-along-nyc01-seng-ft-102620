45498b6bc29690ef9e4fe5810d85c7b6fcc4ca28
Keith Williams <thestreetkw@hotmail.com>
2020-10-12 02:02:36 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,28 +1,40 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {name:
         #your key/value pairs here
+        "Lord Montague", age: "53"
         },
-      :matriarch => {
+      :matriarch => {name:
         #your key/value pairs here
+        "Lady Montague", age: "54"
         },
-      :hero => {
+      :hero => {name:
         #your key/value pairs here
+        "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+   ]
+         }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {name:
         #your key/value pairs here
+        "Lord Capulet", age: "50"
         },
-      :matriarch => {
+      :matriarch => {name:
         #your key/value pairs here
+        "Lady Capulet", age: "51"
         },
-      :heroine => {
+      :heroine => {name:
         #your key/value pairs here
+        "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
    }
   }
 
